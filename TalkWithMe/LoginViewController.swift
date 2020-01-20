60184ef1c952d2f57927ea20da0632c34187663e
Gustavo <gustaa1320@gmail.com>
2020-01-20 11:16:03 -0300

    Adicionei a tela de login

---
+++ TalkWithMe/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  TalkWithMe
+//
+//  Created by Francisco Gustavo on 20/01/20.
+//  Copyright © 2020 Rôney Reis. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
